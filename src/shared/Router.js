e16a3ff334ff9780fee9238a713fd6498a5a2f45
marksenee <mark233891@gmail.com>
2022-09-09 19:19:56 +0900

    [UI] 테스트용 -> 로그인 모달 확인

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "../pages/MainPage";
+import LoginTestPage from "../pages/LoginTestPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        {/* 모달 테스트 페이지 */}
+        <Route path="/test" element={<LoginTestPage />} />
       </Routes>
     </BrowserRouter>
   );
