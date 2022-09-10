f0757a89eec8c232ca6c1fcb0dfc9d0df5ef6480
marksenee <mark233891@gmail.com>
2022-09-10 16:46:06 +0900

    [UI] 검색 페이지 생성 및 라우터 설정

---
+++ src/pages/SearchPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../components/navbar/NavBar";
+import Search from "../components/search/Search";
+
+function SearchPage() {
+  return (
+    <>
+      <NavBar />
+      <Search />
+    </>
+  );
+}
+
+export default SearchPage;
