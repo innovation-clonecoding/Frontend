13bf750037b7d448e0ff2eebaf7d504ce0143a1a
JUNGJIN <ojj824@naver.com>
2022-09-11 01:44:19 +0900

    [UI] Comments Component 제작

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -3,18 +3,20 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from "../pages/MainPage";
 import LoginTestPage from "../pages/LoginTestPage";
 import WritePage from "../pages/WritePage";
+import TestComment from "pages/TestComment";
 
 const Router = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        {/* 모달 테스트 페이지 */}
-        <Route path="/test" element={<LoginTestPage />} />
-        <Route path="/write" element={<WritePage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<MainPage />} />
+				{/* 모달 테스트 페이지 */}
+				<Route path="/test" element={<LoginTestPage />} />
+				<Route path="/write" element={<WritePage />} />
+				<Route path="/comment" element={<TestComment />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default Router;
