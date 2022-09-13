35be69015f3920ece4985c3e288ed34b3d770872
marksenee <mark233891@gmail.com>
2022-09-13 17:16:42 +0900

    [Fix] 헤더 -> token 유무에 따라 로그인 버튼, 드롭다운 버튼 처리

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -2,6 +2,7 @@ import { userApis } from "api/userApi";
 import Dropdown from "components/dropdown/Dropdown";
 import ShowModal from "components/modal/ShowModal";
 import useChange from "hooks/useChange";
+import useToken from "hooks/useToken";
 import React, { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
@@ -10,6 +11,7 @@ function NavBar() {
   const navigate = useNavigate();
   const [dropdownVisibility, setDropdownVisibility] = useState(false);
   const [isModal, ModalHandler] = useChange();
+  const token = useToken();
 
   const onLogout = () => {
     userApis.logout().then((res) => {
@@ -29,32 +31,41 @@ function NavBar() {
             <button>
               <FiSearch className="w-6 h-6" />
             </button>
-            <button className="p-2 m-auto text-white duration-200 bg-black border-2 hover:bg-white hover:text-black rounded-3xl ">
-              새 글 작성
-            </button>
-            <button
-              className="w-20 p-2 m-auto text-white bg-black border-2 rounded-3xl "
-              onClick={() => ModalHandler()}
-            >
-              로그인
-            </button>
-            <ShowModal show={isModal} modalHandler={ModalHandler} />
-            <button onClick={(e) => setDropdownVisibility(!dropdownVisibility)}>
-              {dropdownVisibility ? "Close" : "Open"}
-            </button>
-            <Dropdown visibility={dropdownVisibility}>
-              <div className="">
-                <div
-                  className="flex flex-col items-center justify-center h-10 hover:bg-slate-200 hover:text-green-500"
-                  onClick={() => onLogout()}
+            {!token ? (
+              <>
+                <button
+                  className="w-20 p-2 m-auto text-white bg-black border-2 rounded-3xl "
+                  onClick={() => ModalHandler()}
                 >
-                  로그아웃
-                </div>
-                <div className="flex flex-col items-center justify-center h-10 hover:bg-slate-200 hover:text-green-500">
-                  내 벨로그
-                </div>
-              </div>
-            </Dropdown>
+                  로그인
+                </button>
+                <ShowModal show={isModal} modalHandler={ModalHandler} />
+              </>
+            ) : (
+              <>
+                <button className="p-2 m-auto text-white duration-200 bg-black border-2 hover:bg-white hover:text-black rounded-3xl ">
+                  새 글 작성
+                </button>
+                <button
+                  onClick={(e) => setDropdownVisibility(!dropdownVisibility)}
+                >
+                  {dropdownVisibility ? "Close" : "Open"}
+                </button>
+                <Dropdown visibility={dropdownVisibility}>
+                  <div className="">
+                    <div
+                      className="flex flex-col items-center justify-center h-10 hover:bg-slate-200 hover:text-green-500"
+                      onClick={() => onLogout()}
+                    >
+                      로그아웃
+                    </div>
+                    <div className="flex flex-col items-center justify-center h-10 hover:bg-slate-200 hover:text-green-500">
+                      내 벨로그
+                    </div>
+                  </div>
+                </Dropdown>
+              </>
+            )}
           </div>
         </div>
       </div>
