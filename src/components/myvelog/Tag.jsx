c105b1200e2b66586bc3d6607a03d9bcc4c6344d
marksenee <mark233891@gmail.com>
2022-09-15 14:30:21 +0900

    [Feat] 내 벨로그 -> 내 게시글 조회 & 특정 태그 게시글 조회

--- src/components/myvelog/Tag.jsx
+++ src/components/myvelog/Tag.jsx
@@ -15,16 +15,21 @@ function Tag() {
   const [active, setActive] = useState();
   const [tagName, setTagName] = useState();
   const [myPost, setMyPost] = useState();
+  const [allPost, setAllPost] = useState();
+  const [isAllTag, setIsAllTag] = useState();
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(__getTag(nickName)).then((res) => {
+      setAllPost(res.payload.postList);
       setTag(res.payload.tagList);
       setTagNum(res.payload.numOfTagList);
     });
+    setIsAllTag(true);
   }, [dispatch]);
 
   const onTagClick = (index, nickName, tagName) => {
+    setIsAllTag(false);
     userApis.getTagData(nickName, tagName).then((res) => {
       setMyPost(res);
     });
@@ -32,10 +37,24 @@ function Tag() {
     setTagName(tagName);
   };
 
+  const onAllClick = () => {
+    setIsAllTag(true);
+  };
+
   return (
     <>
       <TagContainer>
-        <ChooseTag>전체보기 ({tag?.length})</ChooseTag>
+        {/* [내 벨로그] 전체 게시글 조회*/}
+        <ChooseTag
+          style={
+            isAllTag
+              ? { background: "#12b886", color: "white" }
+              : { background: "#f8f9fa" }
+          }
+          onClick={() => onAllClick()}
+        >
+          전체보기 ({allPost?.length})
+        </ChooseTag>
         {tag?.map((item, index) => {
           return (
             <TagComponent
@@ -46,30 +65,44 @@ function Tag() {
               onTagClick={onTagClick}
               index={index}
               active={active}
+              isAllTag={isAllTag}
             />
           );
         })}
       </TagContainer>
-      {/* <Contents /> */}
-      {console.log("tagname", tagName)}
+      {/* [내 벨로그] isAllTag true/false에 따라서 전체 게시글 or 특정 태그 게시글 */}
+      {isAllTag
+        ? allPost?.map((item, index) => {
+            return <Contents key={index} item={item} />;
+          })
+        : myPost?.map((item, index) => {
+            return <Contents key={index} item={item} />;
+          })}
     </>
   );
 }
 
-function TagComponent({ index, item, nickName, tagNum, onTagClick, active }) {
+function TagComponent({
+  index,
+  item,
+  nickName,
+  tagNum,
+  onTagClick,
+  active,
+  isAllTag,
+}) {
   return (
     <>
       <NotChooseTag
         onClick={() => onTagClick(index, nickName, item, active)}
         style={
-          active === index
+          active === index && !isAllTag
             ? { background: "#12b886", color: "white" }
             : { background: "#f8f9fa" }
         }
       >
         {item} ({tagNum})
       </NotChooseTag>
-      {/* {active === index } */}
     </>
   );
 }
